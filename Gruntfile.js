43a24c9d31d36250a1a9cc4e665198ebcf54b692
thuy616 <nguyen.n.t.thuy@gmail.com>
2017-04-24 18:30:41 -0700

    :construction: WIP

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,11 +8,22 @@ module.exports = function(grunt) {
 	        'css/fancy.css': 'sass/fancy.scss'
 	      }
 	    }
-		}
+		},
+    watch: {
+        css: {
+            files: ['sass/*.scss'],
+            tasks: ['sass'],
+            options: {
+                spawn: false
+            }
+        }
+    }
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.registerTask('build', [
 		'sass'
 	]);
+  grunt.registerTask('dev', ['watch']);
 }
